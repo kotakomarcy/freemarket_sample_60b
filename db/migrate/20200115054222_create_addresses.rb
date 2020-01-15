7a8d8ee5a80d423cf67b71ffbf17bb6c4c0b45b6
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-15 14:51:00 +0900

    create_address_table
    addressテーブル・addressモデルの作成

---
+++ db/migrate/20200115054222_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string :zip_code, null:false
+      t.string :prefectur, null:false
+      t.string :city, null:false
+      t.string :block, null:false
+      t.string :building_name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_051714) do
+ActiveRecord::Schema.define(version: 2020_01_15_054222) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "zip_code", null: false
+    t.string "prefectur", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
