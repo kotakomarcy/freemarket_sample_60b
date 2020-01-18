fffc033d63ab67eb4cc7755b0d489db70c855640
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-18 19:45:04 +0900

    add_routes_markup
    マークアップ用ルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
+  
   devise_for :users, controllers:{
-    sessions: 'users/sessions',
     registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   resources :signup do
     collection do
       get 'index'
     end
+  end
+
+  devise_scope :user do
+    get 'users/signup/registration', to: 'users/registrations#new'
+    get 'users/signup/phone_num', to: 'users/registrations#new_phone'
+    get 'addresses', to: 'users/registrations#new_address'
+    get 'payments', to: 'users/registrations#new_payment'
+    get 'done', to: 'users/registrations#done'
   end
 
   root "products#index"
