63d4273a513ce763ddaa847edefed6cf5f801be0
kotakomarcy <55982558+kotakomarcy@users.noreply.github.com>
2020-01-18 21:34:51 +0900

    ユーザー新規登録ビュー一式

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,5 @@ class Address < ApplicationRecord
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefectur
+  belongs_to_active_hash :prefecture
 end
