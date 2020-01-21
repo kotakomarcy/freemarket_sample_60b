cb029fc45426292d6aea7ba97403d56e9541ae86
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-21 23:02:12 +0900

    add_products_create_method
    product_controllerにアクション、メソッドを追加
    ・newアクション
    ・createアクション
    ・ストロングパラメーター

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+    @product.product_images.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :size, :condition, :delivery_charge, :delivery_way, :delivery_area, :delivery_days, :price, :status, product_images_attributes: [:image])
   end
 end
