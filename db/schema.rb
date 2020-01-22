b39bd5f9c2c0e558e5d5844fcbaf8b5cbd30c6a4
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-22 19:42:41 +0900

    add_column_user
    productテーブルにuserカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_093709) do
+ActiveRecord::Schema.define(version: 2020_01_22_103935) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_01_21_093709) do
     t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -79,4 +81,5 @@ ActiveRecord::Schema.define(version: 2020_01_21_093709) do
 
   add_foreign_key "payments", "users"
   add_foreign_key "product_images", "products"
+  add_foreign_key "products", "users"
 end
