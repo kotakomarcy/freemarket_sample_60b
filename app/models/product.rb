28c1732e747c342676987f483d5707236088f59e
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-22 22:16:54 +0900

    add_enum_product_size
    productモデルにenumでサイズを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,8 +12,9 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :status, presence: true
 
+  enum size: { 'Sサイズ': 1, 'Mサイズ': 2, 'Lサイズ': 3, 'XLサイズ': 4, 'Freeサイズ': 5 }
   enum condition: { '新品、未使用': 1, '未使用に近い': 2, '目立った傷や汚れなし': 3, 'やや傷や汚れあり': 4, '全体的に状態が悪い': 5 }
-  enum delivery_way:  { '未定': 1 }
   enum delivery_charge: { '送料込み（出品者負担）': 1, '着払い（購入者負担）': 2 }
+  enum delivery_way:  { '未定': 1, 'らくらくメルカリ便': 2, 'ゆうメール': 3, 'クロネコヤマト': 4 }
   enum delivery_days: { '１〜２日で発送': 1, '２〜４日で発送': 2, '４〜７日で発送': 3 }
 end
