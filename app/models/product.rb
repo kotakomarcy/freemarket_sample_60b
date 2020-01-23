67c1f46d2733e6bc56c83a7c6ab751280b1aea6f
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-23 22:00:19 +0900

    add_status_prefecture
    prefectureモデルに内容記述
    productモデルのアソシエーション他、記述追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-  has_many :product_images
-  accepts_nested_attributes_for :product_images, allow_destroy: true
+  extend ActiveHash::Associations::ActiveRecordExtentions
+    has_many :product_images
+    accepts_nested_attributes_for :product_images, allow_destroy: true
+    belongs_to :delivery_area, class_name: 'Prefecture'
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
@@ -17,4 +19,5 @@ class Product < ApplicationRecord
   enum delivery_charge: { '送料込み（出品者負担）': 1, '着払い（購入者負担）': 2 }
   enum delivery_way:  { '未定': 1, 'らくらくメルカリ便': 2, 'ゆうメール': 3, 'クロネコヤマト': 4 }
   enum delivery_days: { '１〜２日で発送': 1, '２〜４日で発送': 2, '４〜７日で発送': 3 }
+  enum status: { '出品中': 0, '出品停止': 1, '交渉中': 2, '売却済': 3 }
 end
