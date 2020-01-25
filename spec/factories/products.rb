2c36d3dd4ba734433b1880e7370899dffb2421dd
makurahumi <ethelandce.01kannna@gmail.com>
2020-01-25 10:41:42 +0900

    add_rspec_testcode
    モデル単体テストのコードを記述

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
   factory :product do
     name             { "黒のジャケット" }
     description      { "３年前に買いましたが、着ないのでお譲りします" }
-    size             { "2" }
-    condition        { "2" }
-    delivery_charge  { "1" }
-    delivery_way     { "4" }
-    delivery_area    { "" }
-    delivery_days    { "2" }
+    size             { "Mサイズ" }
+    condition        { "未使用に近い" }
+    delivery_charge  { "送料込み（出品者負担）" }
+    delivery_way     { "クロネコヤマト" }
+    delivery_area    { "7" }
+    delivery_days    { "２〜４日で発送" }
     price            { "5000" }
-    status           { "" }
+    status           { "出品中" }
     user_id          { "1" }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,64 @@
 require 'rails_helper'
+
 describe Product do
   describe '#create' do
-    it "バリデーション" do
+    it "全て入力すれば登録可能" do
+      product = build(:product)
+      expect(product).to be_valid
+    end
+
+    it "nameが空の場合、エラー" do
+      product = build(:product, name: "")
+      product.valid?
+      expect(product.errors[:name]).to include("can't be blank")
+    end
+
+    it "descriptionが空の場合、エラー" do
+      product = build(:product, description: "")
+      product.valid?
+      expect(product.errors[:description]).to include("can't be blank")
+    end
+
+    it "conditionが空の場合、エラー" do
+      product = build(:product, condition: "")
+      product.valid?
+      expect(product.errors[:condition]).to include("can't be blank")
+    end
+
+    it "delivery_chargeが空の場合、エラー" do
+      product = build(:product, delivery_charge: "")
+      product.valid?
+      expect(product.errors[:delivery_charge]).to include("can't be blank")
+    end
+
+    it "delivery_wayが空の場合、エラー" do
+      product = build(:product, delivery_way: "")
+      product.valid?
+      expect(product.errors[:delivery_way]).to include("can't be blank")
+    end
+
+    it "delivery_areaが空の場合、エラー" do
+      product = build(:product, delivery_area: "")
+      product.valid?
+      expect(product.errors[:delivery_area]).to include("can't be blank")
+    end
+
+    it "delivery_daysが空の場合、エラー" do
+      product = build(:product, delivery_days: "")
+      product.valid?
+      expect(product.errors[:delivery_days]).to include("can't be blank")
+    end
+
+    it "priceが空の場合、エラー" do
+      product = build(:product, price: "")
+      product.valid?
+      expect(product.errors[:price]).to include("can't be blank")
+    end
+
+    it "statusが空の場合、エラー" do
+      product = build(:product, status: "")
+      product.valid?
+      expect(product.errors[:status]).to include("can't be blank")
     end
   end
 end
