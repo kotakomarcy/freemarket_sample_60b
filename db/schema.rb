d35e5ef93db13be79f42a941a5b76b3624ebecec
makurahumi <ethelandce.01kannna@gmail.com>
2020-02-05 23:40:00 +0900

    add_signed_in_method
    サインイン後のビューを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_132545) do
+ActiveRecord::Schema.define(version: 2020_02_05_104038) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_132545) do
     t.integer "condition", null: false
     t.integer "delivery_charge", null: false
     t.integer "delivery_way", null: false
-    t.integer "delivery_area", null: false
+    t.integer "prefecture_id", null: false
     t.integer "delivery_days", null: false
     t.integer "price", null: false
     t.integer "status", null: false
