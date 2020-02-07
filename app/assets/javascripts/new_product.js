c61ecca8d84ac81a9a67e62be45430ffd4568b77
makurahumi <ethelandce.01kannna@gmail.com>
2020-02-07 20:42:48 +0900

    add_preview_js

--- app/assets/javascripts/new_product.js
+++ app/assets/javascripts/new_product.js
@@ -1,12 +1,20 @@
 $(function() {
   //画像用inputを生成する関数
-  const buildFileField = (index)=> {
-    const html = `<div class="js-file_group" data-index="${index}">
+  const buildFileField = (num)=> {
+    const html = `<label class="js-file_group" data-index="${num}" for="product_product_images_attributes_${num}_image">
                     <input class="js-file" type="file"
-                    name="product[product_images_attributes][${index}][image]"
-                    id="product_product_images_attributes_${index}_image">
-                    <br>
-                    <span class="js-remove">削除</span>
+                    name="product[product_images_attributes][${num}][image]"
+                    id="product_product_images_attributes_${num}_image">
+                  </label>`;
+    return html;
+  }
+
+  //プレビュー用のimgタグを生成する関数
+  const buildImg = (index, url)=> {
+    const html = `<div class="previews">
+                    <img data-index="${index}" image="${url}" width="100px" height="100px"></br>
+                    <label class="js-file_group" data-index="${index}" for="product_product_images_attributes_${index}_image">編集</label>
+                    <span class="js-remove" data-index="${index}">削除</span>
                   </div>`;
     return html;
   }
@@ -14,16 +22,41 @@ $(function() {
   // file_fieldのnameに動的なindexをつける
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
+  //既に使われているindexを排除
+  lastIndex = $(".js-file_group:last").data('index');
+  fileIndex.splice(0, lastIndex);
+
+  $('.hidden-destroy').hide();
+
   $('#image-box').on('change', '.js-file', function(e) {
-    // fileIndexの先頭の数字を使って、inputを生成
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    //末尾に１足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    const targetIndex = $(this).parent().data('index');
+    // ファイルのブラウザ上でのURLを取得
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+
+    //該当indexを持つimgタグがあれば取得して変数imgに入れる（画像変更の処理）
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('image', blobUrl);
+    } else { //新規画像の処理
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+      // fileIndexの先頭の数字を使って、inputを生成
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      //末尾に１足した数を追加する
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index');
+    //該当indexを振られているチェックボックスを取得
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    //もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop("checked", true);
+
     $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
+
     //画像入力欄が0にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    binding.pry
+    #binding.pry
     if @product.save
       redirect_to root_path
     else
@@ -21,6 +21,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :size, :condition, :delivery_charge, :delivery_way, :prefecture_id, :delivery_days, :price, :status, product_images_attributes: [:image])
+    params.require(:product).permit(:name, :description, :size, :condition, :delivery_charge, :delivery_way, :prefecture_id, :delivery_days, :price, :status, product_images_attributes: [:image, :_destroy, :id])
   end
 end
