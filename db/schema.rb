b93d2ba485f4966bd986bc9665415f66c3a31d34
kotakomarcy <55982558+kotakomarcy@users.noreply.github.com>
2020-02-12 21:29:44 +0900

    商品出品カラム修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_104038) do
 ActiveRecord::Schema.define(version: 2020_02_08_090256) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
