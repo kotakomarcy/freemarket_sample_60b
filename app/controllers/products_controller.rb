9b29cafce10ed2a24e888373f95b7d162310d394
makurahumi <ethelandce.01kannna@gmail.com>
2020-02-22 16:32:10 +0900

    add_edit_method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+  end
+
   def show
     @product = Product.find(params[:id])
   end
