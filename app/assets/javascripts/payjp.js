66c63520fe0669845950569bed4aed3a090d6092
kotakomarcy <55982558+kotakomarcy@users.noreply.github.com>
2020-02-23 20:58:36 +0900

    クレジットカード登録画面作成

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,32 +1,33 @@
-var form = $("#card__form");
-  Payjp.setPublicKey("sk_test_09c484c2295eb1744967f104");
-//まずはテスト鍵をセットする↑
-  $("#submit_btn").on("click",function(e){
-    e.preventDefault();
-  //↑ここでrailsの処理を止めることでjsの処理を行う
-    var card = {
-      number: $("#card_number").val(),
-      cvc: $("#card_cvc").val(),
-      exp_month: $("#card_month").val(),
-      exp_year: $("#card_year").val()
-    };
-   //↑Pay.jpに登録するデータを準備する
-    Payjp.createToken(card,function(status,response){
-   //↑先ほどのcard情報がトークンという暗号化したものとして返ってくる
-      form.find("input[type=submit]").prop("disabled", true);
-      if(status == 200){//←うまくいった場合200になるので
-        $("#card_number").removeAttr("name");
-        $("#card_cvc").removeAttr("name");
-        $("#card_month").removeAttr("name");
-        $("#card_year").removeAttr("name");
-       //↑このremoveAttr("name")はデータを保持しないように消している
-        var payjphtml = `<input type="hidden" name="payjpToken" value=${response.id}>`
-        form.append(payjphtml);
-        //↑これはdbにトークンを保存するのでjsで作ったトークンをセットしてる
-        document.inputForm.submit();
-       //↑そしてここでsubmit！！これでrailsのアクションにいく！もちろん上でトークンをセットしているのでparamsの中には{payjpToken="トークン"}という情報が入っている
-      }else{
-        alert("カード情報が正しくありません。");
-      }
-    });
-  });
+document.addEventListener(
+  "DOMContentLoaded", e => {
+    if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
+      Payjp.setPublicKey("pk_test_a2782c80d54ae6f19e4c4810"); //ここに公開鍵を直書き
+      let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
+      btn.addEventListener("click", e => { //ボタンが押されたときに動作
+        e.preventDefault(); //ボタンを無効化
+        let card = {
+          number: document.getElementById("number").value,
+          cvc: document.getElementById("cvc").value,
+          exp_month: document.getElementById("exp_month").value,
+          exp_year: document.getElementById("exp_year").value
+        }; //入力されたデータを取得
+        Payjp.createToken(card, (status, response) => {
+          if (status === 200) { //成功時
+            $("#number").removeAttr("name");
+            $("#cvc").removeAttr("name");
+            $("#exp_month").removeAttr("name");
+            $("#exp_year").removeAttr("name"); //データを削除
+            $("#charge-form").append(
+              $('<input type="hidden" name="payjp-token">').val(response.id)
+            ); //取得したトークンを送信できる状態にします
+            $("#charge-form").get(0).submit();
+            alert("登録が完了しました"); //確認用
+          } else {
+            alert("カード情報が正しくありません。"); //確認用
+          }
+        });
+      });
+    }
+  },
+  false
+);
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,72 @@
 class PaymentsController < ApplicationController
   require "payjp"
-
+  
   def new
-    payment = Payment.where(user_id: current_user.id)
-    redirect_to action: "show" if payment.exists?
+    @payment = Payment.where(user_id: current_user.id)
+    redirect_to action: "show" if @payment.exists?
+  end
+
+  def show
+    @payment = Payment.where(user_id: current_user.id).first
+    if @payment.blank?
+    else
+      Payjp.api_key = "sk_test_09c484c2295eb1744967f104"
+      customer = Payjp::Customer.retrieve(@payment.customer_id)
+      @default_card_information = customer.cards.retrieve(@payment.card_id)
+    end
   end
 
   def pay
     Payjp.api_key = "sk_test_09c484c2295eb1744967f104"
-    if params['payjpToken'].blank?
+    if params['payjp-token'].blank?
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-      card: params['payjpToken'],
+      email: current_user.email,
+      card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       )
-      @payment = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      @payment = Payment.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @payment.save
         redirect_to action: "show"
-        flash[:notice] = 'クレジットカードの登録が完了しました'
       else
         redirect_to action: "pay"
-        flash[:alert] = 'クレジットカード登録に失敗しました'
       end
     end
- end
+  end
 
+  def delete
+    payment = Payment.where(user_id: current_user.id).first
+    if payment.blank?
+    else
+      Payjp.api_key = "sk_test_09c484c2295eb1744967f104"
+      customer = Payjp::Customer.retrieve(payment.customer_id)
+      customer.delete
+      payment.delete
+    end
+      redirect_to action: "show"
+  end
 
+  def confirmation
+    @product = Product.find(params[:product_id])
+    @addresses = Address.find_by(user_id: current_user.id)
+    card_information
+  end
+
+  def complete
+    @product = Product.find(params[:product_id])
+    @addresses = Address.find_by(user_id: current_user.id)
+    card_information
+  end
+
+  private
+  def card_exist
+    @payment = Payment.where(user_id: current_user.id).first
+    redirect_to action: "show" if @payment.blank?
+  end
+
+  def card_params
+    params.permit('payjp-token',:product_id)
+  end
+  
 end
