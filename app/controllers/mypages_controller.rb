5a60fe162e6073ded86811cb9383125d2641c7b4
kotakomarcy <55982558+kotakomarcy@users.noreply.github.com>
2020-03-04 17:40:35 +0900

    @userのメソッド追加

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,18 +1,15 @@
 class MypagesController < ApplicationController
   before_action :authenticate_user!
   before_action :profile_params,only: [:update]
-
+  before_action :set_user,only: [:index, :profile, :update]
 
   def index
-    @user = current_user
   end
 
   def profile
-    @user = current_user
   end
 
   def update
-    @user = current_user
     if @user.update(profile_params)
       redirect_to profile_mypages_path
     else
@@ -28,6 +25,10 @@ class MypagesController < ApplicationController
   private
   def profile_params
     params.require(:user).permit(:nickname, :profile_text)
+  end
+
+  def set_user
+    @user = current_user
   end
 
 end
