cd27e6b3b1caf4606962b5f4237b4c71444872da
makurahumi <ethelandce.01kannna@gmail.com>
2020-03-05 20:07:43 +0900

    add_before_action

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,15 +1,14 @@
 class AddressesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_address, only:[:edit, :update]
 
   def edit
     @user = current_user
-    @address = Addresses.find_by(user_id: current_user.id)
   end
 
   def update
-    @address = Address.find_by(user_id: current_user.id)
     if @address.update(set_address)
-      redirect_to address_mypages_path
+      redirect_to mypages_path
     else
       render :edit
     end
@@ -17,7 +16,11 @@ class AddressesController < ApplicationController
 
   private
     def set_address
-      params.permit(:zip_code, :prefecture_id, :city, :block, :building_name).merge(user_id: current_user.id)
+      params.require(:address).permit(:zip_code, :prefecture_id, :city, :block, :building_name).merge(user_id: current_user.id)
+    end
+
+    def find_address
+      @address = Address.find_by(user_id: current_user.id)
     end
 
 end
